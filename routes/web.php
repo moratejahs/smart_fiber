f6acb2ecdd9fde5854b5bfabdb37d7582e007167
moratejahs <jahsmorate@gmail.com>
2025-04-18 12:07:02 +0800

    done set up admin web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboard;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function (){
+    Route::get('dashboard', [AdminDashboard::class, 'index'])->name('admin.dashboard.index');
+    Route::get('users', [UserController::class, 'index'])->name('admin.users.index');
+
 });
